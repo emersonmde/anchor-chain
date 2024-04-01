63845662865c16ad3319cd10cfd8a4d28a624228
emersonmde <emersonmde@protonmail.com>
2024-04-01 23:17:57 +0000

    Deploying to gh-pages from @ emersonmde/anchor-chain@a2968bb79de653249b38d3ac16722969ca3e1811 🚀

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["anchor_chain",{"doc":"","t":"H","n":["main"],"q":[[0,"anchor_chain"],[1,"anyhow"]],"d":[""],"i":[0],"f":"{{}{{d{b}}}}","c":[],"p":[[1,"unit"],[8,"Result",1]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
