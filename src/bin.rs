ecf2cef0d379588e75e60745ee4ba0e31b9b8e43
Matthew Emerson <emersonmde@protonmail.com>
2024-04-02 18:51:35 -0400

    Updated docs for Parallel Node

--- src/bin.rs
+++ src/bin.rs
@@ -3,16 +3,14 @@
 
 extern crate anchor_chain;
 
-use anyhow::Result;
-
 use anchor_chain::{
     chain::ChainBuilder,
-    link::Link,
     models::{claude_3::Claude3Bedrock, gpt_3_5_turbo::Gpt3_5Turbo},
     node::PassthroughNode,
-    parallel_node::{self, ParallelNode},
+    parallel_node::ParallelNode,
     prompt::Prompt,
 };
+use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -20,17 +18,12 @@ async fn main() -> Result<()> {
     let claude3_link =
         Box::new(Claude3Bedrock::new("You are a helpful assistant".to_string()).await);
 
-    let concat_fn: Box<dyn Fn(Vec<String>) -> Result<String> + Send + Sync> =
-        Box::new(|outputs: Vec<String>| {
-            println!("Outputs: {:?}", outputs);
-            Ok(outputs.concat())
-        });
+    let concat_fn = Box::new(|outputs: Vec<String>| {
+        println!("Outputs: {:?}", outputs);
+        Ok(outputs.concat())
+    });
 
-    let parallel_node = ParallelNode::<
-        String,
-        String,
-        Box<dyn Fn(Vec<String>) -> Result<String> + Send + Sync>,
-    >::new(vec![gpt3_link, claude3_link], concat_fn);
+    let parallel_node = ParallelNode::new(vec![gpt3_link, claude3_link], concat_fn);
 
     let chain = ChainBuilder::new(Prompt::new("{input}"))
         .link(parallel_node)
--- src/parallel_node.rs
+++ src/parallel_node.rs
@@ -1,3 +1,46 @@
+//! # Parallel Node
+//!
+//! The `ParallelNode` struct represents a node that processes input through
+//! multiple nodes in parallel. The output of each node is then combined using
+//! a provided function to produce the final output.
+//!
+//! Example:
+//! ```no_run
+//! use anyhow::Result;
+//! use anchor_chain::{
+//!     chain::ChainBuilder,
+//!     models::{claude_3::Claude3Bedrock, gpt_3_5_turbo::Gpt3_5Turbo},
+//!     node::PassthroughNode,
+//!     parallel_node::ParallelNode,
+//!     prompt::Prompt,
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     let gpt3_link = Box::new(Gpt3_5Turbo::new("You are a helpful assistant".to_string()).await);
+//!     let claude3_link =
+//!         Box::new(Claude3Bedrock::new("You are a helpful assistant".to_string()).await);
+//!
+//!     let concat_fn = Box::new(|outputs: Vec<String>| {
+//!         println!("Outputs: {:?}", outputs);
+//!         Ok(outputs.concat())
+//!     });
+//!
+//!     let parallel_node = ParallelNode::new(vec![gpt3_link, claude3_link], concat_fn);
+//!
+//!     let chain = ChainBuilder::new(Prompt::new("{input}"))
+//!         .link(parallel_node)
+//!         .link(PassthroughNode::new())
+//!         .with_trace(true)
+//!         .build();
+//!     chain
+//!         .process("Write a hello world program in Rust".to_string())
+//!         .await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+
 use crate::node::Node;
 use anyhow::Result;
 use async_trait::async_trait;
