0f3d644c730c516ede47de506ef426987754feee
emersonmde <emersonmde@protonmail.com>
2024-04-16 01:43:57 +0000

    Deploying to gh-pages from @ emersonmde/anchor-chain@d4341a4892968e27d918dea5272cf6901457c6cf 🚀

--- anchor_chain/sidebar-items.js
+++ anchor_chain/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["chain","error","link","message","models","node","parallel_node","prompt","vector"]};
+window.SIDEBAR_ITEMS = {"mod":["chain","error","link","logger","message","models","node","parallel_node","prompt","vector"]};
